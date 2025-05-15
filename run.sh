77511d05a0776de096630839d1aa5e6b3a6b4a83
OliEfr <ollihaus@t-online.de>
2025-05-15 14:55:14 +0000

    add run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+python3 run.py --input_video assets/run_804388000/undist_rgb_half_size --output_dir ./outputs/run_804388000 --encoder vitl --grayscale --save_npz
+
+python3 run.py --input_video assets/stand_up_2431270000/undist_rgb_half_size --output_dir ./outputs/stand_up_2431270000 --encoder vitl --grayscale --save_npz
+
+python3 run.py --input_video assets/turn_left_1771233000/undist_rgb_half_size --output_dir ./outputs/turn_left_1771233000 --encoder vitl --grayscale --save_npz
+
+python3 run.py --input_video assets/turn_right_1771233000/undist_rgb_half_size --output_dir ./outputs/turn_right_1771233000 --encoder vitl --grayscale --save_npz
+
+python3 run.py --input_video assets/walk_869488000/undist_rgb_half_size --output_dir ./outputs/walk_869488000 --encoder vitl --grayscale --save_npz
